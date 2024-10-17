d530942a6f062ed2bce91d1c29fe06e0fcb899d0
NicolasS1990 <dericosanz@gmail.com>
2024-10-17 15:01:41 -0300

    Se agrego archivo .js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*console.log("hola")
+const nombre = prompt("ingrese un nombre");
+console.log(nombre)
+
+alert("Bienvenido "  + nombre)*/
+
+// Ejemplo con decimales 
+
+
+/*const precio1 = parseFloat (prompt(" Ingrese precio 1 del supermercado"));
+const precio2 = parseFloat (prompt("Ingrese precio 2 del supermercado"));
+
+console.log();
+console.log(precio2);
+
+const suma = precio1 + precio2;
+
+console.log("La suma es: " + suma);*/  
