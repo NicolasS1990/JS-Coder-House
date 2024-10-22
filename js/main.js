26c5d1f7a678e6f627a627894cd5cc9292a4e49b
NicolasS1990 <dericosanz@gmail.com>
2024-10-22 11:55:07 -0300

    operadores y condicionales

--- js/main.js
+++ js/main.js
@@ -16,3 +16,50 @@ console.log(precio2);
 const suma = precio1 + precio2;
 
 console.log("La suma es: " + suma);*/  
+
+/* IF */
+
+/*const nombre = prompt("Ingrese un nombre");
+
+if (nombre == "Pepe"){
+    console.log("Hola pepe");
+}else {
+    console.log("Hola persona que no es Pepe");
+}*/
+
+/*if....else if*/
+
+/*const precio = parseInt(prompt ("Ingrese un precio"))
+
+if (precio == 100){
+    alert ("valor correcto")
+}else if ( precio < 100){
+    alert ("el precio tiene que ser mayor")
+}else{
+    alert ("Precio mayor a 100")
+    console.log(precio);
+}
+*/
+/*
+const nombre = "Pepe";
+const edad = 25;
+
+if ((nombre == "Pepe")&&(edad == 20)) {
+    console.log("El nombre es Pepe y su edad es 20");
+ 
+}else{
+    console.log("El nombre no es Pepe o no tiene 20");
+}*/
+
+
+const nombre = prompt ("Ingrese un nombre"); // Pedro o Juan
+const edad = parseInt (prompt("Ingrese la edad")); // >=18
+
+if ((nombre == "Juan" || nombre == "Pedro") && (edad >= 18)){
+    console.log("Hola " +nombre );
+}else {
+    console.log("Acceso denegado");
+}
+
+const numero = 10; 
+const noEsNumero10 = 
